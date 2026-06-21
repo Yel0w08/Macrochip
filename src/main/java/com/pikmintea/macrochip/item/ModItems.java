2d53d91027131a5653d50e13f0c967b2e5b1eef2
Yel0w <144521481+Yel0w08@users.noreply.github.com>
2026-06-21 15:45:56 +0200

    Update ModItems.java

--- src/main/java/com/pikmintea/macrochip/item/ModItems.java
+++ src/main/java/com/pikmintea/macrochip/item/ModItems.java
@@ -1,4 +1,10 @@
 package com.pikmintea.macrochip.item;
 
+import com.pikmintea.macrochip.Macrochip;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModItems {
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Macrochip.MODID);
+    
 }
